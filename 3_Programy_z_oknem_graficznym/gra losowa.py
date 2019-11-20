078ac6dc6fb4c1af49d8c1cc8c0b05776eb54acd
pm1975 <pm1975@windowslive.com>
2019-11-20 23:57:58 +0100

    strona 28 koniec gry logicznej

--- 3_Programy_z_oknem_graficznym/gra losowa.py
+++ 3_Programy_z_oknem_graficznym/gra losowa.py
@@ -5,6 +5,26 @@ t = Tk()
 t.title("Wybierz przycisk")
 t.geometry("300x350")
 
+def trafiony():
+    for i in przyciski:
+        i.destroy()
+    global etykieta
+    etykieta = Label(t, text = "Trafiłeś dobry przycisk")
+    etykieta.pack(fill=BOTH,expand=YES)
+    t.after(5000,restart)
+
+def restart():
+    etykieta.destroy()
+    wstaw_przyciski()
+
+def pudlo():
+    for i in przyciski:
+        i.destroy()
+    global etykieta
+    etykieta = Label(t, text = "Trafiłeś zły przycisk")
+    etykieta.pack(fill=BOTH,expand=YES)
+    t.after(500,restart)
+
 def wstaw_przyciski():
     ile_przyciskow = 8
     global przyciski
@@ -17,3 +37,5 @@ def wstaw_przyciski():
             przyciski.append(Button(t, text = "kliknij mnie", command=pudlo))
     for i in przyciski:
         i.pack(fill=BOTH, expand=YES)
+
+wstaw_przyciski()
