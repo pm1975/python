a8f1389bf60a02af2d33ad85713815baa33e0df7
PM <pm1975@windowslive.com>
2019-11-20 17:27:57 +0100

    page 26

--- 3 Programy z oknem graficznym/gra losowa.py
+++ 3 Programy z oknem graficznym/gra losowa.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 t = Tk()
 t.title("Wybierz przycisk")
@@ -6,4 +7,16 @@ t.geometry("300x350")
 
 def wstaw_przyciski():
     ile_przyciskow = 8
+    global przyciski
+    przyciski = []
+    dobry = random.randint(0,ile_przyciskow-1)
+    for i in range (ile_przyciskow):
+        if i == dobry:
+            przyciski.append(Button(t, text = "kliknij mnie", command=trafiony))
+        else:
+            przyciski.append(Button(t, text = "kliknij mnie", command=pudlo))
+    for i in przyciski:
+        i.pack(fill=BOTH, expand=YES)
+        
+        
     
