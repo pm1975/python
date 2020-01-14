c1e29a52c6ebad63bfa3f1b09d625cd1013daff3
PM <pm1975@windowslive.com>
2020-01-14 17:04:21 +0100

    To page 34

--- 28 gra helikopterrr/helikopterrr.py
+++ 28 gra helikopterrr/helikopterrr.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 
@@ -9,10 +10,21 @@ screen = pygame.display.set_mode((szer,wys))
 def napisz(tekst, x, y, rozmiar) :
     cz = pygame.font.SysFont("Arial", rozmiar)
     rend = cz.render(tekst, 1, (255,100,100))
-    x = (szer - rend.get_rect().width)/2
-    y = (wys - rend.get_rect().height)/2
+    #x = (szer - rend.get_rect().width)/2
+    #y = (wys - rend.get_rect().height)/2
     screen.blit(rend, (x,y))
 
-napisz("Naciśnij spację, aby zacząć",80,150,20)
+copokazuje = "menu"
 
-pygame.display.update()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    if copokazuje == "menu":
+        napisz("Naciśnij spację, aby zacząć",80,150,20)
+        grafika = pygame.image.load(os.path.join('logo.png'))
+        screen.blit(grafika, (80,110))
+
+    pygame.display.update()
